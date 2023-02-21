6e8549ba35f409ddf869eb32b309a54e32e34044
Leo Dion <leogdion@brightdigit.com>
2023-02-21 12:20:32 -0500

    adding xcode project

---
+++ KeychainSyncDemo/KeychainSyncDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainSyncDemoApp.swift
+//  KeychainSyncDemo
+//
+//  Created by Leo Dion on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct KeychainSyncDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeychainSyncDemoWatch Watch App/KeychainSyncDemoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainSyncDemoWatchApp.swift
+//  KeychainSyncDemoWatch Watch App
+//
+//  Created by Leo Dion on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct KeychainSyncDemoWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
