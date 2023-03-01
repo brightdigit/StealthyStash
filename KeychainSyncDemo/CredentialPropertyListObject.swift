56bd51d8f3a9471b6ebd3762aeb80f00cd38db6c
Leo Dion <leogdion@brightdigit.com>
2023-03-01 08:27:34 -0500

    adding generic password

---
+++ KeychainSyncDemo/CredentialPropertyListObject.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Combine
+import FloxBxAuth
+
+class CredentialPropertyListObject : ObservableObject {
+  internal init(repository: CredentialsRepository, internetPasswords: [AnyCredentialProperty]? = nil) {
+    self.repository = repository
+    self.credentialProperties = internetPasswords
+    
+    let queryPublisher = self.querySubject      
+      .tryMap(self.repository.query(_:))
+      .share()
+    
+    
+    queryPublisher
+      .map{_ in Optional<Error>.none}
+      .catch{Just(Optional<Error>.some($0))}
+      .compactMap{ $0 as? KeychainError }
+      .receive(on: DispatchQueue.main)
+      .assign(to: &self.$lastError)
+    
+    queryPublisher
+      .map(Optional<[AnyCredentialProperty]>.some)
+      .replaceError(with: nil)
+      .receive(on: DispatchQueue.main)
+      .assign(to: &self.$credentialProperties)
+  }
+  
+  let repository : CredentialsRepository
+  @Published var credentialProperties: [AnyCredentialProperty]?
+  let querySubject  = PassthroughSubject<Query, Never> ()
+  @Published var lastError : KeychainError?
+  
+
+  func query (_ query: Query) {
+    self.querySubject.send(query)
+  }
+}
--- KeychainSyncDemo/Preview Content/InternetPasswordData.swift
+++ KeychainSyncDemo/Preview Content/InternetPasswordData.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 
-struct InternetPasswordData : Codable {
+struct CredentialPropertyData : Codable {
   let account : String
   let data : String
   let accessGroup : String?
   let createdAt: Date
   let modifiedAt : Date
   let description: String?
+  let service: String?
+  let comments: String?
   let type : Int?
   let label: String?
   let url : URL?
@@ -23,20 +25,66 @@ struct InternetPasswordData : Codable {
 }
 
 
-extension InternetPasswordData {
+extension CredentialPropertyData {
   static let collection = {
     let decoder = JSONDecoder()
-    let url = Bundle.main.url(forResource: "internet-passwords", withExtension: "json")
+    let url = Bundle.main.url(forResource: "credential-properties", withExtension: "json")
     let data = try! Data(contentsOf: url!)
     decoder.dateDecodingStrategy = .secondsSince1970
-    return try! decoder.decode([InternetPasswordData].self, from:  data)
+    return try! decoder.decode([CredentialPropertyData].self, from:  data)
   }()
 }
 
-extension InternetPasswordItem {
-  static let previewCollection = {
-    InternetPasswordData.collection.map(
-      InternetPasswordItem.init
+
+
+extension GenericPasswordItem {
+  init(data: CredentialPropertyData) {
+    self.init(
+      account: data.account,
+      data: data.data.data(using: .utf8)!,
+      service: data.service,
+      accessGroup: data.accessGroup,
+      createdAt: data.createdAt,
+      modifiedAt: data.modifiedAt,
+      description: data.description,
+      type: data.type,
+      label: data.label,
+      isSynchronizable: data.isSynchronizable
     )
-  }()
+  }
+}
+
+extension InternetPasswordItem {
+  init(data: CredentialPropertyData) {
+    self.init(
+      account: data.account,
+      data: data.data.data(using: .utf8)!,
+      accessGroup: data.accessGroup,
+      createdAt: data.createdAt,
+      modifiedAt: data.modifiedAt,
+      description: data.description,
+      type: data.type,
+      label: data.label,
+      server: data.url?.host,
+      protocol: data.url?.scheme.flatMap(ServerProtocol.init(scheme:)),
+      port: data.url?.port,
+      path: data.url?.path,
+      isSynchronizable: data.isSynchronizable
+    )
+  }
+}
+
+
+extension AnyCredentialProperty {
+  init(data: CredentialPropertyData) {
+    let property : any CredentialProperty
+    if data.service != nil {
+      property = GenericPasswordItem(data: data)
+    } else {
+      property = InternetPasswordItem(data: data)
+    }
+    self.init(property: property)
+  }
+  
+  static let previewCollection = CredentialPropertyData.collection.map(Self.init(data: ))
 }
