41b15dd406d6bd9da9a2b3bf78bc7be1901d7d4f
Leo Dion <leogdion@brightdigit.com>
2023-03-21 22:14:52 -0400

    moving files to spm, and fixing UI tests

---
+++ Tests/StealthyStashTests/FloxBxAuthTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import StealthyStash
+
+final class FloxBxAuthTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        // XCTAssertEqual(FloxBxAuth().text, "Hello, World!")
+    }
+}
