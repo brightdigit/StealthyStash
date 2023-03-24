8e77ac0305a2d0dc7bcdcb6d241a72f25f3b9a04
Leo Dion <leogdion@brightdigit.com>
2023-03-23 21:03:14 -0400

    merge README files

--- Sample/App/CompositeSecretObject.swift
+++ Sample/App/CompositeSecretObject.swift
@@ -1,45 +1,39 @@
-//
-//  CompositeSecretObject.swift
-//  KeychainSyncDemo
-//
-//  Created by Leo Dion on 3/22/23.
-//
-
-import Foundation
 import Combine
+import Foundation
 import StealthyStash
 
 class TriggerSet {
   let saveCompletedTrigger = PassthroughSubject<Void, Never>()
-  //private let receivedUpdate = PassthroughSubject<Void, Never>()
-  
-  var receiveUpdatePublisher : AnyPublisher<Void, Never> {
-    return saveCompletedTrigger.share().eraseToAnyPublisher()
+  // private let receivedUpdate = PassthroughSubject<Void, Never>()
+
+  var receiveUpdatePublisher: AnyPublisher<Void, Never> {
+    saveCompletedTrigger.share().eraseToAnyPublisher()
   }
 }
-class CompositeSecretObject : ObservableObject {
+
+class CompositeSecretObject: ObservableObject {
   internal init(
     repository: SecretsRepository,
-    triggerSet : TriggerSet,
+    triggerSet: TriggerSet,
     secret: CompositeSecretBuilder = CompositeSecretBuilder()
   ) {
     self.repository = repository
     self.secret = secret
-    
-    let resetSourcePublisher = self.resetPassthrough
-      .map{self.source}
-    
-    Publishers.Merge(resetSourcePublisher, self.$source)
+
+    let resetSourcePublisher = resetPassthrough
+      .map { self.source }
+
+    Publishers.Merge(resetSourcePublisher, $source)
       .map(CompositeSecretBuilder.init)
       .receive(on: DispatchQueue.main)
-      .assign(to: &self.$secret)
-    
-    let loadResult = Publishers.Merge(self.loadPassthrough, triggerSet.receiveUpdatePublisher)
+      .assign(to: &$secret)
+
+    let loadResult = Publishers.Merge(loadPassthrough, triggerSet.receiveUpdatePublisher)
       .map {
         Future<CompositeCredentials?, Error> { completed in
           Task {
             do {
-              let secret : CompositeCredentials? = try await self.repository.fetch()
+              let secret: CompositeCredentials? = try await self.repository.fetch()
               completed(.success(secret))
             } catch {
               completed(.failure(error))
@@ -49,31 +43,29 @@ class CompositeSecretObject : ObservableObject {
       }
       .switchToLatest()
       .share()
-    
+
     loadResult
-      .map{_ in true}
+      .map { _ in true }
       .replaceError(with: true)
       .receive(on: DispatchQueue.main)
-      .assign(to: &self.$isLoaded)
-    
-    
+      .assign(to: &$isLoaded)
+
     loadResult
-      .map{_ in Optional<Error>.none}
-      .catch{Just(Optional<Error>.some($0))}
-      .compactMap{ $0 as? KeychainError }
+      .map { _ in Error?.none }
+      .catch { Just(Error?.some($0)) }
+      .compactMap { $0 as? KeychainError }
       .receive(on: DispatchQueue.main)
-      .assign(to: &self.$lastError)
-      
+      .assign(to: &$lastError)
+
     loadResult
       .replaceError(with: nil)
       .receive(on: DispatchQueue.main)
-      .assign(to: &self.$source)
-    
-    
+      .assign(to: &$source)
+
     let saveResult = savePassthrough
-      .map{ self.secret }
+      .map { self.secret }
       .compactMap(CompositeCredentials.init(builder:))
-      .tryMap{ model in
+      .tryMap { model in
         if let source = self.source {
           try self.repository.update(from: source, to: model)
         } else {
@@ -82,54 +74,53 @@ class CompositeSecretObject : ObservableObject {
         return model
       }
       .share()
-      
-  
+
     saveResult
-      .map{_ in Optional<Error>.none}
-      .catch{Just(Optional<Error>.some($0))}
-      .compactMap{ $0 as? KeychainError }
+      .map { _ in Error?.none }
+      .catch { Just(Error?.some($0)) }
+      .compactMap { $0 as? KeychainError }
       .receive(on: DispatchQueue.main)
-      .assign(to: &self.$lastError)
-      
+      .assign(to: &$lastError)
+
     let saveSuccess = saveResult
-      .map{$0 as CompositeCredentials?}
-      .catch{_ in Just(Optional<CompositeCredentials>.none)}
-      .compactMap{$0}
+      .map { $0 as CompositeCredentials? }
+      .catch { _ in Just(CompositeCredentials?.none) }
+      .compactMap { $0 }
       .share()
-    
+
     saveSuccess
-      .map{$0 as CompositeCredentials?}
+      .map { $0 as CompositeCredentials? }
       .receive(on: DispatchQueue.main)
-      .assign(to: &self.$source)
-    
+      .assign(to: &$source)
+
     saveSuccessCancellable = saveSuccess
       .breakpoint()
-      .map{_ in ()}
+      .map { _ in () }
       .subscribe(triggerSet.saveCompletedTrigger)
   }
-  
-  let repository : SecretsRepository
-  
+
+  let repository: SecretsRepository
+
   let resetPassthrough = PassthroughSubject<Void, Never>()
   let savePassthrough = PassthroughSubject<Void, Never>()
   let loadPassthrough = PassthroughSubject<Void, Never>()
-  
-  @Published var lastError : KeychainError?
-  @Published var source : CompositeCredentials?
+
+  @Published var lastError: KeychainError?
+  @Published var source: CompositeCredentials?
   @Published var secret = CompositeSecretBuilder()
   @Published var isLoaded = false
-  
-  var saveSuccessCancellable : AnyCancellable!
-  
+
+  var saveSuccessCancellable: AnyCancellable!
+
   func save() {
-    self.savePassthrough.send()
+    savePassthrough.send()
   }
-  
-  func reset () {
-    self.resetPassthrough.send()
+
+  func reset() {
+    resetPassthrough.send()
   }
-  
-  func load () {
-    self.loadPassthrough.send()
+
+  func load() {
+    loadPassthrough.send()
   }
 }
--- Sources/StealthyStash/Auth/InternetPasswordItem.swift
+++ Sources/StealthyStash/Auth/InternetPasswordItem.swift
@@ -1,99 +1,55 @@
-import Security
 import Foundation
+import Security
 
-public struct InternetPasswordItem : Identifiable, Hashable, SecretProperty{
-  
+public struct InternetPasswordItem: Identifiable, Hashable, SecretProperty {
   public func uniqueAttributes() -> SecretDictionary {
-    
-      /*
-       For internet passwords, the primary keys include kSecAttrAccount, kSecAttrSecurityDomain, kSecAttrServer, kSecAttrProtocol, kSecAttrAuthenticationType, kSecAttrPort, and kSecAttrPath
-       */
-    return [
-      kSecAttrAccount as String: self.account,
-      kSecAttrAccessGroup as String: self.accessGroup?.nilTrimmed(),
-      
-      //kSecAttrSecurityDomain as String, self.account
-      kSecAttrServer as String: self.server,
+    [
+      kSecAttrAccount as String: account,
+      kSecAttrAccessGroup as String: accessGroup,
+      kSecAttrServer as String: server,
       kSecAttrProtocol as String: self.protocol,
-      //kSecAttrAuthenticationType as String, self.account
-      kSecAttrPort as String: self.port,
-      kSecAttrPath as String: self.path
+      kSecAttrPort as String: port,
+      kSecAttrPath as String: path
     ]
   }
-  
+
   public func otherProperties() -> SecretDictionary {
     [
-      kSecAttrSynchronizable as String: self.isSynchronizable,
-      kSecAttrDescription as String : description?.nilTrimmed(),
-      kSecAttrType as String : type,
-      kSecAttrLabel as String : label,
+      kSecAttrSynchronizable as String: isSynchronizable,
+      kSecAttrDescription as String: description,
+      kSecAttrType as String: type,
+      kSecAttrLabel as String: label
     ]
   }
-//  
-//  public func deleteQuery() -> [String : Any?] {
-//    let addQuery = addQuery()
-//    let uniqueKeys = [kSecAttrService as String, kSecAttrAccount as String]
-//    var query = [String : Any?]()
-//    var attributes = [String : Any?]()
-//    for (key, value) in addQuery {
-//      if uniqueKeys.contains(key) {
-//        query[key] = value
-//      } else {
-//        attributes[key] = value
-//      }
-//    }
-//    query[kSecClass as String] = Self.propertyType.secClass
-//    return query
-//  }
-//  
-//  public func updateQuerySet() -> UpdateQuerySet {
-//    let addQuery = addQuery()
-//    let uniqueKeys = [kSecAttrServer as String, kSecAttrAccount as String]
-//    var query = [String : Any?]()
-//    var attributes = [String : Any?]()
-//    for (key, value) in addQuery {
-//      if uniqueKeys.contains(key) {
-//        query[key] = value
-//      } else {
-//        attributes[key] = value
-//      }
-//    }
-//    query[kSecClass as String] = Self.propertyType.secClass
-//    return .init(query: query, attributes: attributes, id: self.id)
-//  }
-  
+
   public static let propertyType: SecretPropertyType = .internet
-  
+
   public var id: String {
-    
-    [self.account,
-    self.server,
+    [account,
+     server,
      self.protocol?.rawValue,
-    self.authenticationType,
-     self.port?.description,
-     self.path].compactMap{$0}.joined()
+     authenticationType,
+     port?.description,
+     path].compactMap { $0 }.joined()
   }
-  
-//
-//  public func addQuery () -> [String : Any?]
-//  {
-//    [
-//     kSecClass as String: kSecClassInternetPassword,
-//     kSecAttrAccount as String: self.account,
-//     kSecValueData as String: self.data,
-//     kSecAttrServer as String: self.server?.nilTrimmed(),
-//     kSecAttrAccessGroup as String: self.accessGroup?.nilTrimmed(),
-//     kSecAttrSynchronizable as String: self.isSynchronizable,
-//     kSecAttrDescription as String : description?.nilTrimmed(),
-//     kSecAttrType as String : type,
-//     kSecAttrLabel as String : label,
-//     kSecAttrProtocol as String : self.protocol?.cfValue,
-//     kSecAttrAuthenticationType as String : authenticationType,
-//     kSecAttrPort as String : port,
-//     kSecAttrPath as String : path
-//   ]
-//  }
-  public init(account: String, data: Data, accessGroup: String? = nil, createdAt: Date? = nil, modifiedAt: Date? = nil, description: String? = nil, comment : String? = nil, type: Int? = nil, label: String? = nil, server: String? = nil, `protocol`: ServerProtocol? = nil, authenticationType: AuthenticationType? = nil, port: Int? = nil, path: String? = nil, isSynchronizable: Bool? = nil) {
+
+  public init(
+    account: String,
+    data: Data,
+    accessGroup: String? = nil,
+    createdAt: Date? = nil,
+    modifiedAt: Date? = nil,
+    description: String? = nil,
+    comment: String? = nil,
+    type: Int? = nil,
+    label: String? = nil,
+    server: String? = nil,
+    protocol: ServerProtocol? = nil,
+    authenticationType: AuthenticationType? = nil,
+    port: Int? = nil,
+    path: String? = nil,
+    isSynchronizable: Bool? = nil
+  ) {
     self.account = account
     self.data = data
     self.accessGroup = accessGroup
@@ -104,35 +60,46 @@ public struct InternetPasswordItem : Identifiable, Hashable, SecretProperty{
     self.type = type
     self.label = label
     self.server = server
-    self.`protocol` = `protocol`
+    self.protocol = `protocol`
     self.authenticationType = authenticationType
     self.port = port
     self.path = path
     self.isSynchronizable = isSynchronizable
   }
-  
-  public let account : String
-  public let data : Data
-   
-  public let accessGroup : String?
-  public let createdAt : Date?
-  public let modifiedAt : Date?
+
+  public let account: String
+  public let data: Data
+
+  public let accessGroup: String?
+  public let createdAt: Date?
+  public let modifiedAt: Date?
   public let description: String?
-  public let comment : String?
-  public let type : Int?
-  public let label : String?
-  public let server : String?
-  public let `protocol` : ServerProtocol?
-  public let authenticationType : AuthenticationType?
+  public let comment: String?
+  public let type: Int?
+  public let label: String?
+  public let server: String?
+  public let `protocol`: ServerProtocol?
+  public let authenticationType: AuthenticationType?
   public let port: Int?
   public let path: String?
-  public let isSynchronizable : Bool?
+  public let isSynchronizable: Bool?
 }
 
-
 extension InternetPasswordItem {
-  
-  public init(account: String, data: Data, accessGroup: String? = nil, url: URL? = nil, createdAt: Date? = nil, modifiedAt: Date? = nil, description: String? = nil, comment : String? = nil, type: Int? = nil, label: String? = nil, authenticationType: AuthenticationType? = nil, isSynchronizable: Bool? = nil) {
+  public init(
+    account: String,
+    data: Data,
+    accessGroup: String? = nil,
+    url: URL? = nil,
+    createdAt: Date? = nil,
+    modifiedAt: Date? = nil,
+    description: String? = nil,
+    comment _: String? = nil,
+    type: Int? = nil,
+    label: String? = nil,
+    authenticationType _: AuthenticationType? = nil,
+    isSynchronizable: Bool? = nil
+  ) {
     self.init(
       account: account,
       data: data,
@@ -143,28 +110,28 @@ extension InternetPasswordItem {
       type: type?.trimZero(),
       label: label,
       server: url?.host,
-      protocol: url.flatMap{$0.scheme}.flatMap(ServerProtocol.init(scheme:)),
+      protocol: url.flatMap(\.scheme).flatMap(ServerProtocol.init(scheme:)),
       port: url?.port?.trimZero(),
       path: url?.path,
       isSynchronizable: isSynchronizable
     )
   }
-  
-  public init(dictionary : [String : Any]) throws {
-    let account : String = try dictionary.require(kSecAttrAccount)
-    let data : Data = try dictionary.require(kSecValueData)
-    let accessGroup : String? = try dictionary.requireOptional(kSecAttrAccessGroup)
-    let createdAt : Date? = try dictionary.requireOptional(kSecAttrCreationDate)
-    let modifiedAt : Date? = try dictionary.requireOptional(kSecAttrModificationDate)
+
+  public init(dictionary: [String: Any]) throws {
+    let account: String = try dictionary.require(kSecAttrAccount)
+    let data: Data = try dictionary.require(kSecValueData)
+    let accessGroup: String? = try dictionary.requireOptional(kSecAttrAccessGroup)
+    let createdAt: Date? = try dictionary.requireOptional(kSecAttrCreationDate)
+    let modifiedAt: Date? = try dictionary.requireOptional(kSecAttrModificationDate)
     let description: String? = try dictionary.requireOptional(kSecAttrDescription)
-    let type : Int? = try dictionary.requireOptionalCF(kSecAttrType)
-    let label : String? = try dictionary.requireOptionalCF(kSecAttrLabel)
-    let server : String? = try dictionary.requireOptional(kSecAttrServer)
-    let protocolString : CFString? = try dictionary.requireOptional(kSecAttrProtocol)
-    let `protocol` : ServerProtocol? = protocolString.flatMap(ServerProtocol.init(number: ))
+    let type: Int? = try dictionary.requireOptionalCF(kSecAttrType)
+    let label: String? = try dictionary.requireOptionalCF(kSecAttrLabel)
+    let server: String? = try dictionary.requireOptional(kSecAttrServer)
+    let protocolString: CFString? = try dictionary.requireOptional(kSecAttrProtocol)
+    let `protocol`: ServerProtocol? = protocolString.flatMap(ServerProtocol.init(number:))
     let port: Int? = try dictionary.requireOptional(kSecAttrPort)
     let path: String? = try dictionary.requireOptional(kSecAttrPath)
-    let isSynchronizable : Bool? = try dictionary.requireOptional(kSecAttrSynchronizable)
+    let isSynchronizable: Bool? = try dictionary.requireOptional(kSecAttrSynchronizable)
     self.init(
       account: account,
       data: data,
@@ -182,4 +149,3 @@ extension InternetPasswordItem {
     )
   }
 }
-
